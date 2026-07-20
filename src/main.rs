3268e0b4987c764ca26d5d7e8fb8558e7f9c2acd
Sajjad Ahmad Almas <sajjadalmas443@gmail.com>
2026-07-20 06:40:24 -0700

    Learned custom functions and return values in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-
 fn main() {
-    let mut x: i32 = 5;
-    println!("X ki value hai: {}", x);
+    // Function ko call kar rahe hain
+    say_hello();
+    
+    // Values pass karke add function call kar rahe hain
+    let sum = add(15, 25);
+    println!("Dono numbers ka sum hai: {}", sum);
+}
 
-    x = 10;
-    println!("Nayi value hai: {}", x);
+// Chhota sa custom function
+fn say_hello() {
+    println!("Solana Rust Developer Journey!");
+}
+
+// Parameters lene wala aur value return karne wala function
+fn add(a: i32, b: i32) -> i32 {
+    a + b // Rust mein aakhri line par semicolon (;) na lagayein to wo return ho jata hai
 }
